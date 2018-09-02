016efcc72ae79c076d3bec585321b5aa0fe789d3
Lean Junio <leanjunio@live.com>
2018-09-02 00:24:50 -0400

    Begin Workshop_2

---
+++ OOP345/Workshop_2/Workshop_2/Text.cpp
@@ -0,0 +1,18 @@
+// Name: Lean Junio
+// Seneca Student ID: 019-109-123
+// Seneca email: ljjunio@myseneca.ca
+// Date of completion:
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
+#include "Text.h"
+
+Text::Text()
+{
+}
+
+
+Text::~Text()
+{
+}
---
+++ OOP345/Workshop_2/Workshop_2/Text.h
@@ -0,0 +1,21 @@
+// Name: Lean Junio
+// Seneca Student ID: 019-109-123
+// Seneca email: ljjunio@myseneca.ca
+// Date of completion:
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
+#ifndef _W2_TEXT_H
+#define _W2_TEXT_H
+
+class Text
+{
+public:
+	Text();
+	~Text();
+};
+
+#endif // !_W2_TEXT_H
+
+
