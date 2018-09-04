0416dbc5cfb5159d6e47f9265a97f8e4d836bd67
Lean Junio <leanjunio@live.com>
2018-09-04 18:34:10 -0400

    Added necessary comments

--- OOP345/Workshop_1/Workshop_1/process.cpp
+++ OOP345/Workshop_1/Workshop_1/process.cpp
@@ -1,3 +1,11 @@
+// Name: Lean Junio
+// Seneca Student ID: 019-109-123
+// Seneca email: ljjunio@myseneca.ca
+// Date of completion:
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
 #include <iostream>
 #include "CString.h"
 #include "process.h"
