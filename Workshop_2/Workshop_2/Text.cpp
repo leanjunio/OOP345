c5b31d186186ea6479fcfe838186c6774555c8f6
Lean Junio <leanjunio@live.com>
2018-09-06 08:53:42 -0400

    Fixed Dates

--- Workshop_2/Workshop_2/Text.cpp
+++ Workshop_2/Workshop_2/Text.cpp
@@ -1,7 +1,7 @@
 // Name: Lean Junio
 // Seneca Student ID: 019-109-123
 // Seneca email: ljjunio@myseneca.ca
-// Date of completion:
+// Date of completion: September 6, 2018
 //
 // I confirm that the content of this file is created by me,
 // with the exception of the parts provided to me by my professor.
