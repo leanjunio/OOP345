1b6df6118faab07fa07d10df8b841bb4edf07a1d
Lean Junio <leanjunio@live.com>
2018-09-18 16:45:43 -0400

    commit list

--- WS03/WS03/List.h
+++ WS03/WS03/List.h
@@ -6,10 +6,22 @@ class List
 {
 	T m_ListArr[N];							// Array of any data type of dimension 'N'
 public:
-	List();
-	size_t size() const;					// returns the number of entries in the array
-	const T& operator[](int index) const;	// returns the element of 'index' requested
-	void operator+=(const T&);				// Adds an element to the array if space is available
+	List()
+		: m_ListArr{ nullptr }
+	{
+	}
+	size_t size() const						// returns the number of entries in the array
+	{
+		
+	}
+	const T& operator[](int index) const	// returns the element of 'index' requested
+	{
+		return m_ListArr[index];
+	}
+	void operator+=(const T&)				// Adds an element to the array if space is available
+	{
+
+	}
 };
 
 #endif // !_LIST_H
