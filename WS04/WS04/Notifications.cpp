c48a8fe5633d854315420929a0fcf4d70bd78539
Lean Junio <leanjunio@live.com>
2018-09-19 11:28:19 -0400

    Update notes for polymorphism

--- WS04/WS04/Notifications.cpp
+++ WS04/WS04/Notifications.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "Notifications.h"
 #include "Message.h"
 
 namespace w4
 {
+	//
 	Notifications::Notifications()
 	{
-		m_Messages->clear();
+		
 	}
 
 	Notifications::Notifications(const Notifications & other)
+		// Allow the current object to take the value of the parameter passed to the constructor
+		// This constructor can be called via: `Notifications n1 = Notifications::previousN1;`
 	{
 		*this = other;
 	}
 
 	Notifications & Notifications::operator=(const Notifications & other)
+		// Allow the current object to take the value of the parameter using the '=' operator
+		// Called via: `Notifications::n1 = Notifications::n2;`
 	{
 		if (this != &other)
 		{
-			size_t v_Size = other.m_Messages->size();
-
-			for (size_t i = 0; i < v_Size; i++)
-				this->m_Messages[i] = other.m_Messages[i];
+			
 		}
 
 		return *this;
--- WS04/WS04/Notifications.h
+++ WS04/WS04/Notifications.h
@@ -1,3 +1,5 @@
+// Notifications.h
+// Manages access to a set of up to 10 instances of the Message type
 #ifndef _W4_NOTIFICATIONS_H
 #define _W4_NOTIFICATIONS_H
 
@@ -6,11 +8,9 @@
 
 namespace w4
 {
-	const int MAX_OBJECTS = 10;
-
 	class Notifications
 	{
-		std::vector<Message> m_Messages[MAX_OBJECTS];
+		std::vector<Message> m_Messages;
 	public:
 		Notifications();
 		Notifications(const Notifications&);
