7cd5ec3145c3a5ee7200b7cab9a000a69170703b
Lean Junio <leanjunio@live.com>
2018-10-03 08:20:08 -0400

    reserved 10 spaces for vectors

---
+++ WS04/WS04/Files/Message.h
@@ -0,0 +1,37 @@
+// Name: Lean Junio
+// Seneca Student ID: 019109123
+// Seneca email: ljjunio@myseneca.ca	
+// Date of completion: 10/1/2018
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
+#ifndef _W4_MESSAGE_H
+#define _W4_MESSAGE_H
+
+#include <fstream>
+#include <string>
+
+using String = std::string;
+
+namespace w4
+{
+	class Message
+	{
+		String m_User;
+		String m_Tweet;
+		String m_Reply;
+	public:
+		Message();
+		Message(String user, String tweet, String reply);
+		Message(std::ifstream& in, char c);
+		~Message();
+		bool empty() const;
+		void display(std::ostream& os) const;
+	};
+}
+
+#endif // !_W4_MESSAGE_H
+
+
+
