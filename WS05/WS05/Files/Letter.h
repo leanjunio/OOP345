2a9e24c8db78641c330dbc43292ecb56282ad9e9
Lean Junio <leanjunio@live.com>
2018-10-05 21:21:10 -0400

    Adding Files for Matrix on WS05

---
+++ WS05/WS05/Files/Letter.h
@@ -0,0 +1,26 @@
+// Name: Lean Junio
+// Seneca Student ID: 019-109-123
+// Seneca email: ljjunio@myseneca.ca
+// Date of completion: 
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
+#ifndef _SICT_LETTER_H
+#define _SICT_LETTER_H
+
+namespace sict
+{
+	/// Enumeration constant that identifies the letter grades in a course
+	enum Letter { Aplus, A, Bplus, B, Cplus, C, Dplus, D, F };
+
+	/// A template function that converts each enumeration constant into a C-style null-terminated string
+	/// Receive an unmodifiable reference to one of the enumeration constants
+	/// Returns the address of string that represents that constant
+	template <typename T>
+	
+
+}
+
+#endif // !_SICT_LETTER_H
+
