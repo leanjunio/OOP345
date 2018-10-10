65e2d9b640fd91b12916e35e2f87e76244423e11
Lean Junio <leanjunio@live.com>
2018-10-10 16:29:45 -0400

    cleanup

--- WS05/WS05/Letter.h
+++ WS05/WS05/Letter.h
@@ -11,12 +11,12 @@
 
 namespace sict
 {
-	/// Enumeration constant that identifies the letter grades in a course
+	// Enumeration constant that identifies the letter grades in a course
 	enum class Letter { Aplus, A, Bplus, B, Cplus, C, Dplus, D, F };
 
-	/// A template function that converts each enumeration constant into a C-style null-terminated string
-	/// Receive an unmodifiable reference to one of the enumeration constants
-	/// Returns the address of string that represents that constant
+	// A template function that converts each enumeration constant into a C-style null-terminated string
+	// Receive an unmodifiable reference to one of the enumeration constants
+	// Returns the address of string that represents that constant
 	template <typename T>
 	const char* convert(const Letter& letter)
 	{
@@ -43,7 +43,6 @@ namespace sict
 
 		return grade;
 	}
-	
 }
 
 #endif // !_SICT_LETTER_H
