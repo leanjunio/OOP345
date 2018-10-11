9cacf37d24745fe5ebf097b6b83beb3f9a24e609
Lean Junio <ljjunio@myseneca.ca>
2018-10-11 14:53:03 -0400

    restructure

--- WS05/WS05/Grades.cpp
+++ WS05/WS05/Grades.cpp
@@ -1,6 +1,6 @@
 // Name: Lean Junio
 // Seneca Student ID: 019-109-123
-// Seneca email: ljjunio@myseneca.ca
+// Seneca email: ljjunio@myseneca.ca 
 // Date of completion: 10/11/2018
 //
 // I confirm that the content of this file is created by me,
@@ -15,10 +15,10 @@
 
 namespace sict
 {
-	void Grades::displayGrades(std::ostream & os, std::string (*letter)(float)) const
+	void Grades::displayGrades(std::ostream & os, std::string (*letter)(float)) const 
 	{
 		for (size_t i = 0; i < static_cast<size_t>(m_LineCount); i++)
-			os << std::setw(10) << m_StudentNumbers[i] << " " << std::setw(5) << std::fixed << std::setprecision(2) << m_Grades[i] << " " << std::setw(4) << letter(m_Grades[i]) << " " << std::endl;
+			os << std::setw(10) << m_StudentNumbers[i] << " " << std::setw(5) << std::fixed << std::setprecision(2) << m_Grades[i] << " " << std::setw(4) << letter(m_Grades[i]) << std::endl;
 	}
 	Grades::Grades(const char* file)
 	{	
