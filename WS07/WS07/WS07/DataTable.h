d7c2c3e2d4d3496128a41c32e2ecdf1438ee036a
Lean Junio <leanjunio>
2018-11-07 08:58:44 -0500

    Add members for width and precision

--- WS07/WS07/WS07/DataTable.h
+++ WS07/WS07/WS07/DataTable.h
@@ -27,26 +27,24 @@ namespace w7
 	class DataTable
 	{
 		std::vector<std::pair<T, T>> m_DataSet;
+
+		int m_width;
+		int m_precision;
 	public:
 
 		// Receives a reference to the file, field width, number of decimals to display
 		// The object retrieves data from the file and stores them as members
 		//
 		DataTable(std::ifstream& file, int width, int precision)
+			: m_width(width),
+			  m_precision(precision)
 		{
 			if (file.is_open())
 			{
 				float x, y;
-				int count = 0;
 
 				while (file >> x >> y)
-				{
-					// std::cout << "X: " << x << std::setprecision(precision) << " - Y: " << y << std::setprecision(precision) << std::endl;
 					m_DataSet.push_back(std::make_pair(x, y));
-				}
-
-				std::cout << "V Size: " << m_DataSet.size() << std::endl;
-				// std::cout << count << std::endl;
 			}
 
 		}	
@@ -55,28 +53,56 @@ namespace w7
 
 		}
 
-		// returns the number of points in the dataset (the number of pairs)
-		size_t getSize() const
+		// returns the mean value of the dependent coordinate
+		//
+		T mean() const
 		{
-			return m_DataSet.size();
+
+		}
+
+		// returns the standard deviation of the dependent coordinates
+		//
+		T sigma() const
+		{
+
+		}
+			
+		// returns the median value of the dependent coordinate
+		//
+		T median() const
+		{
+
 		}
 
 		// sets the parameters to the slope and intercept for the data set
+		//
 		void regression(T& slope, T& y_intercept) const
 		{
 
 		}
 
-		/*void display(std::ostream& os) const
+		// displays the data pairs as shown below
+		//
+		void display(std::ostream& os) const
 		{
 			os << m_X << " " m_Y << std::endl;
 		}
 
+		// returns the number of points in the dataset (the number of pairs)
+		//
+		size_t getSize() const
+		{
+			return m_DataSet.size();
+		}
+
+		// calls the DataTable::display(...)
+		//
 		std::ostream& operator<<(std::ostream& os, const DataTable& data)
 		{
+			std::cout << setw(m_width) << "x" << setw(m_width) << "y" << std::endl;
 			data.display(os);
 			return os;
-		}*/
+		}
 	};
 }
 
