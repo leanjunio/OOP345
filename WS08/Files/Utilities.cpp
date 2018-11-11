1becb610043ec557c99db6f066be2a578e413901
Lean Junio <leanjunio@live.com>
2018-11-10 20:28:00 -0500

    Build setup files for VS Code

---
+++ WS08/Files/Utilities.cpp
@@ -0,0 +1,29 @@
+#include <memory>
+#include "List.h"
+#include "Element.h"
+#include "Utilities.h"
+
+using namespace std;
+
+namespace w8
+{
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price)
+	{
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+
+		return priceList;
+	}
+
+	List<Product> mergeSmart(const List<Description>& desc, const List<Price>& price)
+	{
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using smart pointers
+
+
+		return priceList;
+	}
+}
