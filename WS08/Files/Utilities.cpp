051eac94737c2134339fb2f26bc29967cd054441
Lean Junio <leanjunio@live.com>
2018-11-12 17:28:00 -0500

    create mergeSmart

--- WS08/Files/Utilities.cpp
+++ WS08/Files/Utilities.cpp
@@ -37,9 +37,19 @@ namespace w8
 	List<Product> mergeSmart(const List<Description>& desc, const List<Price>& price)
 	{
 		List<Product> priceList;
-		// TODO: Add your code here to build a list of products
-		//         using smart pointers
-
+		
+		for(size_t i = 0; i < desc.size(); i++)
+		{
+			for(size_t n = 0; n < price.size(); n++)
+			{
+				if (desc[i].code == price[n].code)
+				{
+					std::unique_ptr<Product> ptr(new Product(desc[i].desc, price[n].price));
+					ptr->validate();
+					priceList += ptr;
+				}
+			}
+		}
 
 		return priceList;
 	}
