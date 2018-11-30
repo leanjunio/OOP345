0672da3cdaa4182a2fac606783ccdfeaf76abaad
Lean Junio <leanjunio@live.com>
2018-11-30 11:55:16 -0500

    upload LineManager.cpp

--- Milestone/LineManager.cpp
+++ Milestone/LineManager.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <algorithm>
+#include "Utilities.h"
 #include "LineManager.h"
 
 // str - contains the filename specified by the user to be used for linking the assembly line objects (ex. AssemblyLine.dat)
@@ -16,10 +18,50 @@ LineManager::LineManager(const std::string& str, std::vector<Task*> al_addresses
 
     if (file.is_open())
     {
+        std::string buf;
+        int line = 0;
+
+        // Data
+        std::string currentLine;
+        std::string nextItem;
+        std::string currentItem;
+
         m_cntCustomerOrder = co_tobefilled.size();  // get the CustomerOrder count
         for (size_t i = 0; i < m_cntCustomerOrder; ++i)
-            
+            ToBeFilled.push_back(std::move(co_tobefilled[i]));
+
+        while(std::getline(file, buf))
+            line++;
+
+        Utilities localUtility;
+        bool more = true;
+        size_t pos = 0;
+
+        for (size_t i = 0; i < line; ++i)
+        {
+            std::getline(file, currentLine, '\n');
+            currentItem = localUtility.extractToken(currentLine, pos, more);
+
+            if (more)
+            {
+                nextItem = localUtility.extractToken(currentLine, pos, more);
+                for (auto& current: AssemblyLine)
+                    if (current->getName() == currentItem)
+                        currentItem = "";
+                for (auto& firstTask: al_addresses)
+                    if (firstTask->getName() == currentItem)
+                        AssemblyLine.push_back(firstTask);
+                for (auto& secondTask: al_addresses)
+                    if (nextItem == secondTask->getName())
+                        AssemblyLine.push_back(secondTask);
+
+                AssemblyLine[i]->setNextTask(*AssemblyLine[i + 1]);
+            }
+        }
     }
+
+    for (auto& i: AssemblyLine)
+        std::cout << i->getName() << std::endl;
 }
 
 // Performs one cycle of operations on the assembly line
