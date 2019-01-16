873184e35cf953c5d9a5902c0677d5caf64588dc
Lean Junio <leanjunio@live.com>
2019-01-16 08:12:14 -0500

    changed cli command output

--- OOP345SAA/Labs/Lab1/w1 in_lab/w1_in_lab/w1_in_lab/w1.cpp
+++ OOP345SAA/Labs/Lab1/w1 in_lab/w1_in_lab/w1_in_lab/w1.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
 	std::cout << "Command Line : " << argv[0];
-	for (int i = 1; i < argc; i++)
+	for (int i = 0; i < argc; i++)
 		std::cout << argv[i] << ' ';
 
 	// If no parameters are passed, return 1
