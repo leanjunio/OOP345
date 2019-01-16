f77931c4bada4206835c895722ec60c3c841e477
Lean Junio <leanjunio@live.com>
2019-01-16 08:08:54 -0500

    revised main func

--- OOP345SAA/Labs/Lab1/w1 in_lab/w1_in_lab/w1_in_lab/w1.cpp
+++ OOP345SAA/Labs/Lab1/w1 in_lab/w1_in_lab/w1_in_lab/w1.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 	std::cout << "Command Line : " << argv[0];
 	for (int i = 1; i < argc; i++)
 		std::cout << " " << argv[i];
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 
 	// If no parameters are passed, return 1
 	if (argc < 2)
