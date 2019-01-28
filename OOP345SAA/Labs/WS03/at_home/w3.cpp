ec74ab049ba15ddc24627ef9cd5a2a6088e967bd
Lean Junio <leanjunio@live.com>
2019-01-28 11:40:02 -0500

    Created the pairs necessary for the workshop

--- OOP345SAA/Labs/WS03/at_home/w3.cpp
+++ OOP345SAA/Labs/WS03/at_home/w3.cpp
@@ -30,6 +30,7 @@ int main(int argc, char* argv[]) {
 	// each LVPair consists of a label of type std::string and a value of type std::string
 	// the maximum number of elements in the list is 50
 	//
+	List<LVPair<std::string, std::string>, 50> references;
 	std::ifstream index(argv[1]);
 	if (!index) {
 		std::cerr << "*** failed to open index file ***\n";
@@ -63,6 +64,7 @@ int main(int argc, char* argv[]) {
 	// each LVPair consists of a label of type std::string and a value of type int
 	// the maximum number of elements in the list is 50
 	//
+	List<LVPair<std::string, int>, 50> ticketSales;
 	std::ifstream sales(argv[2]);
 	if (!sales) {
 		std::cerr << "*** failed to open sales file ***\n";
