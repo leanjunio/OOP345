3547aaac2f9e2a0f1fb6bfe1c68f3b1b3f1e4c68
Lean Junio <leanjunio@live.com>
2019-01-29 08:13:42 -0500

    Update SummableLVPair

--- OOP345SAA/Labs/WS03/at_home/LVPair.h
+++ OOP345SAA/Labs/WS03/at_home/LVPair.h
@@ -54,20 +54,35 @@ namespace sict
 	template <typename L, typename V>
 	class SummableLVPair : LVPair
 	{
-		V m_initialValueForSummation;
-		size_t m_minFieldWidth = 0;
+		// holds the initial value for starting a summation (this depends on the type of the value in the label-value pair
+		V m_initialValueForSummation = 0;
+
+		// holds the minimum field width for pretty columnar output of label-value pairs
+		// this is the minimum number of characters needed to display any of the labels in a set of labels
+		size_t m_minFieldWidth;
 	public:
 
-		// leaves the object in a safe empty state
-		SummableLVPair()
+		// Template specialization for LVPair<std::string, int>
+		template<>
+		SummableLVPair<std::string, int>()
+			:
 		{
 		}
 
-		// calls the base class 2-argument constructor, passes the values received 
-		// to the base class and increases the stored field width if it is less than 
-		// the return of characters required to display the label for all LVPair objects
+
+		// A templated declaration that initializes the field width class variable to 0
+		// leaves the object in a safe empty state
+		SummableLVPair()
+			: m_initialValueForSummation{}
+			, m_minFieldWidth(0)
+		{
+		}
+
+		// calls the base class 2-argument constructor and passes the values received to the base class
+		// increases the stored field width if it is less than the return of characters required to display the label for all LVPair objects
 		// This class assumes that the type of the first parameter has a member function named size(), which returns that value
 		SummableLVPair(const L& label, const V& v)
+			: LVPair(label, v)
 		{
 		}
 
