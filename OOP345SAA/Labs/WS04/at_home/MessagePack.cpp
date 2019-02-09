64f4ec7509297d2130bce5c4e6399fca41b2f9b2
Lean Junio <leanjunio@live.com>
2019-02-09 15:48:45 -0500

    Finished MessagePack module

---
+++ OOP345SAA/Labs/WS04/at_home/MessagePack.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <utility>
+#include "Message.h"
+#include "MessagePack.h"
+
+namespace sict
+{
+	MessagePack::MessagePack()
+		: m_messages{ nullptr }
+		, m_numElementsInPack{ 0 }
+	{
+	}
+	MessagePack::MessagePack(const Message *arrMessages, int numElementsInArrayPassed)
+	{
+		if (numElementsInArrayPassed > 0 && arrMessages != nullptr)
+		{
+			m_messages = new Message[numElementsInArrayPassed];
+			m_numElementsInPack = { numElementsInArrayPassed };
+
+			for (int i = 0; i < numElementsInArrayPassed; ++i)
+			{
+				if (!arrMessages[i].empty())
+				{
+					m_messages[i] = arrMessages[i];
+				}
+				else
+				{
+					m_messages[i] = Message();
+				}
+			}
+		}
+	}
+	MessagePack::MessagePack(const MessagePack& other)
+	{
+		*this = other;
+	}
+	MessagePack & MessagePack::operator=(const MessagePack& other)
+	{
+		if (this != &other)
+		{
+			delete[] m_messages;
+			m_numElementsInPack = other.m_numElementsInPack;
+
+			for (int i = 0; i < m_numElementsInPack; i++)
+			{
+				m_messages[i] = other.m_messages[i];
+			}
+		}
+		return *this;
+	}
+	MessagePack::MessagePack(MessagePack&& other)
+	{
+		*this = std::move(other);
+	}
+	MessagePack& MessagePack::operator=(MessagePack&& other)
+	{
+		if (this != &other)
+		{
+			delete[] m_messages;
+
+			m_messages = other.m_messages;
+			m_numElementsInPack = other.m_numElementsInPack;
+
+			other.m_messages = { nullptr };
+			other.m_numElementsInPack = { 0 };
+		}
+		return *this;
+	}
+	MessagePack::~MessagePack()
+	{
+		delete[] m_messages;
+		m_messages = nullptr;
+	}
+	void MessagePack::display(std::ostream& os)
+	{
+	}
+	size_t MessagePack::size() const
+	{
+		return size_t(m_numElementsInPack);
+	}
+	std::ostream& operator<<(std::ostream& os, const MessagePack& MPack)
+	{
+		return os;
+	}
+}
--- OOP345SAA/Labs/WS04/at_home/MessagePack.h
+++ OOP345SAA/Labs/WS04/at_home/MessagePack.h
@@ -1,21 +1,28 @@
 #ifndef SICT_MESSAGEPACK
 #define SICT_MESSAGEPACK
 
-#include "Message.h";
+#include <iostream>
+#include "Message.h"
 
 namespace sict
 {
 	class MessagePack
 	{
+		Message* m_messages;
+		int m_numElementsInPack;
 	public:
 		MessagePack();
-		MessagePack(const Message*, );
+		MessagePack(const Message*, int);
 		MessagePack(const MessagePack&);
 		MessagePack& operator=(const MessagePack&);
 		MessagePack(MessagePack&&);
 		MessagePack& operator=(MessagePack&&);
 		~MessagePack();
+		void display(std::ostream& os);
+		size_t size() const;
 	};
+
+	std::ostream& operator<<(std::ostream&, const MessagePack&);
 }
 
 #endif // !SICT_MESSAGEPACK
