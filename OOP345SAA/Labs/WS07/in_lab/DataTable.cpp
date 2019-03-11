d112dedf98b3b31edefda189177798ccc02d0bdf
Lean Junio <leanjunio@live.com>
2019-03-11 10:47:28 -0400

    Finish first draft DataTable::ctor()

--- OOP345SAA/Labs/WS07/in_lab/DataTable.cpp
+++ OOP345SAA/Labs/WS07/in_lab/DataTable.cpp
@@ -1,19 +1,28 @@
-#include "DataTable.h"
-
-extern int FW;
-extern int ND;
-
-namespace sict
-{
-	DataTable::DataTable(std::ifstream)
-	{
-	}
-	void DataTable::displayData(std::ostream &)
-	{
-	}
-	void DataTable::displayStatistics(std::ostream &)
-	{
-	}
-}
+//#include "DataTable.h"
+//
+//extern int FW;
+//extern int ND;
+//
+//namespace sict
+//{
+//	DataTable::DataTable(std::ifstream& file)
+//	{
+//		if (file)
+//		{
+//			bool keepReading = true;
+//
+//			do
+//			{
+//			
+//			} while (keepReading == true);
+//		}
+//	}
+//	void DataTable::displayData(std::ostream &)
+//	{
+//	}
+//	void DataTable::displayStatistics(std::ostream &)
+//	{
+//	}
+//}
 
 
--- OOP345SAA/Labs/WS07/in_lab/DataTable.h
+++ OOP345SAA/Labs/WS07/in_lab/DataTable.h
@@ -3,15 +3,48 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <utility>
 
-namespace sict
-{
-	class DataTable
-	{
+namespace sict {
+	/**
+	 * A class that takes care of holding and processing statistical data
+	 * The table structure will be defined using a vector container holding pair elements in each element of the vector
+	 * std::vector<std::pair<T,T>> will be the structure
+	 * 
+	*/
+	template<class T>
+	class DataTable	{
+		std::vector<std::pair<T,T>> m_dataTableNodes;
 	public:
-		explicit DataTable(std::ifstream);
-		void displayData(std::ostream&);
-		void displayStatistics(std::ostream&);
+
+		/**
+		 * One-arg constructor that reads every record from the file
+		 * The file follows the format[X_Y], _ denotes the space
+		 * The data will be stored in a pair, which is then stored in an index within the vector
+		*/
+		explicit DataTable(std::ifstream& file)	{
+			bool keepReading = true;
+			if (file) {
+				T x = {0};
+				T y = {0};
+				do {
+					file >> x >> y;
+					if (x && y)
+						m_dataTableNodes.push_back(std::make_pair(x,y));					
+					else
+						keepReading = false;
+				} while (keepReading);
+				
+				file >> x >> y;
+			}
+		}
+		void displayData(std::ostream& os)	{
+
+		}
+		void displayStatistics(std::ostream& os) {
+
+		}
 	};
 }
 #endif // !_SICT_DATATABLE_H
