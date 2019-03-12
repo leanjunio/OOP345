c13ef90c38110187dd94a19335add089deeaf638
Lean Junio <leanjunio@live.com>
2019-03-12 11:50:48 -0400

    create mean() and sum()

--- OOP345SAA/Labs/WS07/in_lab/DataTable.h
+++ OOP345SAA/Labs/WS07/in_lab/DataTable.h
@@ -9,6 +9,7 @@
 #include <numeric>
 #include <algorithm>
 #include <cmath>
+#include <string>
 
 /**
  * External variables coming from w7.cpp
@@ -21,11 +22,40 @@ namespace sict {
 	 * A class that takes care of holding and processing statistical data
 	 * The table structure will be defined using a vector container holding pair elements in each element of the vector
 	 * std::vector<std::pair<T,T>> will be the structure
-	 * 
 	*/
 	template<class T>
 	class DataTable	{
-		std::vector<std::pair<T,T>> m_dataTableNodes;
+		std::vector<std::pair<T,T>> m_data;
+	private:
+		/**
+		 * private method that calculates the mean of all the Y coordinates
+		 * The mean can be calculated using the following formula:
+		 * mean = sum of all the integers on the Y/ num of integers on the Y
+		*/
+		T mean() const {
+			T total = sum("second");
+			return total/m_data.size();
+		}
+
+		/**
+		 * Private method that calculates the sum of the chosen coordinates (first, second)
+		 * by using the STL algorithm: accumulate(begin, end, initialValue, [](currentValue with init, currentValueInVector))
+		 * Explanation:
+		 * begin/end - range of the vector to iterate through
+		 * initialValue - 'static_cast<T>(0)' -> make the initial value 0
+		 * currentValue with init - the value starts with 0 and increases based on the returned value of the lambda
+		 * currentValueInVector - the current element
+		*/
+		T sum(std::string& pos) {
+			T total = {0};
+			if (pos == "first") {
+				total += std::accumulate(m_data.begin(), m_data.end(), static_cast<T>(0), [](auto& a, auto* b) { return a + b.first });
+			}
+			if (pos == "second") {
+				total += std::accumulate(m_data.begin(), m_data.end(), static_cast<T>(0), [](auto& a, auto* b) { return a + b.second });
+			}
+			return total;
+		}
 	public:
 
 		/**
@@ -40,7 +70,7 @@ namespace sict {
 				T y = {0};
 				do {
 					if (file >> x >> y) 
-						m_dataTableNodes.push_back(std::make_pair(x,y));
+						m_data.push_back(std::make_pair(x,y));
 					else
 						keepReading = false;
 				} while (keepReading);
@@ -51,8 +81,8 @@ namespace sict {
 		 * Query function that displays the x-y data within the vector in the following format:
 		 * 2.1000 8.0000
 		 * 2.5000 12.0000
-         * 4.0000 14.0000
-         * 3.6000 10.0000
+     * 4.0000 14.0000
+     * 3.6000 10.0000
 		 * 
 		 * This can be done by utilizing the FW and ND provided from the main module
 		 * The method iterates element by element through the vector displaying one pair at a time.
@@ -63,7 +93,7 @@ namespace sict {
 			os << "------------" << std::endl;
 			os << std::setw(FW) << "x" << std::setw(FW) << "y" << std::endl;
 
-			for (const auto& i : m_dataTableNodes)
+			for (const auto& i : m_data)
 				os << std::fixed << std::setprecision(ND) << std::setw(FW) << std::right << std::get<0>(i) << std::setprecision(ND) << std::setw(FW) << std::get<1>(i) << std::endl;
 		}
 
@@ -71,39 +101,31 @@ namespace sict {
 		 * Query that displays the statistics for the current object in the following format:
 		 * y mean    = 11.0000
 		 * y sigma   =  2.5820 
-		 * 
-		 * The mean can be calculated using the following formula:
-		 * mean = sum of all the integers on the Y/ num of integers on the Y
 		*/
 		void displayStatistics(std::ostream& os) {
 			os << "Statistics" << std::endl;
 			os << "----------" << std::endl;
-				
-			// Calculating mean
-			T sum_y = std::accumulate(m_dataTableNodes.begin(), m_dataTableNodes.end(), static_cast<T>(0), [](auto& a, auto& b) { return a + b.second; });
-			size_t num_of_elements = m_dataTableNodes.size();
-			T mean = sum_y/num_of_elements;
+			
+			os << std::fixed << std::setprecision(ND) << "y mean" << std::setw(FW) << "=" << mean() << std::endl;
 
 			// Calculating sigma
 			T total = {};
 			
 			// Create a vector containing all Ys
 			std::vector<T> seconds = {};
-
-			for (const auto& i : m_dataTableNodes)
+			for (const auto& i : m_data)
 				seconds.push_back(std::get<1>(i));
 
-			std::for_each(seconds.begin(), seconds.end(), [&](T& el) 
+			// for each y, 
+			std::for_each(seconds.begin(), seconds.end(), [&](T& current_y) 
 			{ 
-				total += std::pow(el - mean, 2);
+				total += std::pow(current_y - mean, 2);
 			});
 
-			T sigma = std::sqrt(total / (m_dataTableNodes.size() - 1));
+			T sigma = std::sqrt(total / (m_data.size() - 1));
 
-			os << std::fixed << std::setprecision(ND) << "y mean" << std::setw(FW) << "=" << mean << std::endl;
 			os << std::fixed << std::setprecision(ND) << "y sigma" << std::setw(FW) << "=" << sigma << std::endl;
 		}
 	};
 }
 #endif // !_SICT_DATATABLE_H
-
