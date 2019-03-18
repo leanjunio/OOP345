6a3a0892ea0b22d1fed90ae8bdd09b4aa22619bb
Lean Junio <leanjunio@live.com>
2019-03-18 15:21:11 -0400

    Edit skeleton

--- OOP345SAA/OOP-Project/MS2/CustomerOrder.cpp
+++ OOP345SAA/OOP-Project/MS2/CustomerOrder.cpp
@@ -1,5 +1,63 @@
 #include "CustomerOrder.h"
 
 namespace sict {
+	/**
+	 * Default Constructor that sets the object to a safe empty state
+	*/
 	CustomerOrder::CustomerOrder() : m_ItemInfo() {}
-} // namespace sict
+	/**
+	 * One argument constructor that receives a string that contains at least 3 tokens:
+	 * - Customer's name
+	 * - Name of the product being assembled
+	 * - 3..n are names of the items to be added to the Product throughout the assembly process
+	 * If no items are requested to be added (no products are present within the string), throw an exception that says so
+	 * If items are present, allocate memory for each one
+	*/
+	CustomerOrder::CustomerOrder(const std::string &) {
+	}
+
+	/**
+	 * Destructor that deallocates memory
+	*/
+	CustomerOrder::~CustomerOrder() {
+	}
+
+	/**
+	 * Modifier that checks each item request, fills it if the item is available and the request has not been filled and reports the filling in the following format:
+	 * Filled CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] OR
+	 * Unable to fill CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] already filled
+	 * Unable to fill CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] out of stock 
+	 * Also decrements the item stock by one
+	*/
+	void CustomerOrder::fillItem(ItemSet &, std::ostream &)	{
+	}
+
+	/**
+	 * Searches the list of items requested and returns true if all have ben filled
+	*/
+	bool CustomerOrder::isFilled() const {
+		return false;
+	}
+
+	/**
+	 * Receives an item name
+	 * search the list for that ItemName, return true if all requests for the item has been filled or if the item is not in the lists
+	*/
+	bool CustomerOrder::isItemFilled(const std::string &) const {
+		return false;
+	}
+
+	/**
+	 * Returns the name of the customer and their product in the following format:
+	 * CUSTOMER [PRODUCT]
+	*/
+	std::string CustomerOrder::getNameProduct() const {
+		return std::string();
+	}
+
+	/**
+	 * Inserts the data for the current object into the ostream
+	*/
+	void CustomerOrder::display(std::ostream & os, bool showDetail) const {
+	}
+}
