2be39c28aad70675fa773e07f58c5f32c042d989
Lean Junio <leanjunio@live.com>
2019-03-18 10:52:52 -0400

    Fixed mergedRaw

--- OOP345SAA/Labs/WS08/in_lab/Utilities.cpp
+++ OOP345SAA/Labs/WS08/in_lab/Utilities.cpp
@@ -18,15 +18,13 @@ namespace sict {
 	  */
 	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
 		List<Product> priceList;
-		// TODO: Add your code here to build a list of products
-		//         using raw pointers
-		
 		for (size_t i = 0; i < desc.size(); ++i) {
 			for (size_t x = 0; x < price.size(); ++x) {
 				if (desc[i].code == price[x].code) {
 					Product* p = new Product(desc[i].desc, price[x].price);
 					p->validate();
 					priceList += p;
+					delete p;	// deallocate p
 				}
 			}
 		}
