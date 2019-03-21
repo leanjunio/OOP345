83140dc61a79c9576ccc21f85d9042f0aa4aef5a
Lean Junio <leanjunio@live.com>
2019-03-21 00:41:45 -0400

    Cleanup

--- OOP345SAA/OOP-Project/MS2/CustomerOrder.cpp
+++ OOP345SAA/OOP-Project/MS2/CustomerOrder.cpp
@@ -1,14 +1,25 @@
-#include <string>
+#include <vector>
 #include <iomanip>
 #include <algorithm>
 #include "CustomerOrder.h"
+#include "Utilities.h"
 
 namespace sict {
 
 	size_t CustomerOrder::m_fieldWidth = 0;
 
+	/**
+	 * Default Constructor that sets the object to a safe empty state
+	*/
 	CustomerOrder::CustomerOrder() : m_itemInfo{ nullptr }, m_customerName{ "" }, m_productName{ "" }, m_numItems{ 0 } {}
-
+	/**
+	 * One argument constructor that receives a string that contains at least 3 tokens:
+	 * - Customer's name
+	 * - Name of the product being assembled
+	 * - 3..n are names of the items to be added to the Product throughout the assembly process
+	 * If no items are requested to be added (no products are present within the string), throw an exception that says so
+	 * If items are present, allocate memory for each one
+	*/
 	CustomerOrder::CustomerOrder(const std::string& record) : CustomerOrder() {
 		Utilities utility;
 		
@@ -31,8 +42,7 @@ namespace sict {
 		}	
 	}
 
-	CustomerOrder::CustomerOrder(CustomerOrder&& other)
-	{
+	CustomerOrder::CustomerOrder(CustomerOrder&& other) {
 		*this = std::move(other);
 	}
 
@@ -48,36 +58,64 @@ namespace sict {
 		return *this;
 	}
 
+	/**
+	 * Destructor that deallocates memory
+	*/
 	CustomerOrder::~CustomerOrder() {
 		delete[] m_itemInfo;
 		m_itemInfo = { nullptr };
 	}
 
-	void CustomerOrder::fillItem(ItemSet &, std::ostream &)
-	{
+	/**
+	 * Modifier that checks each item request, fills it if the item is available and the request has not been filled and reports the filling in the following format:
+	 * Filled CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] OR
+	 * Unable to fill CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] already filled
+	 * Unable to fill CUSTOMER [PRODUCT][ITEM][SERIAL NUMBER] out of stock
+	 * Also decrements the item stock by one
+	*/
+	void CustomerOrder::fillItem(ItemSet&, std::ostream&) {
 	}
 
-	bool CustomerOrder::isFilled() const
-	{
+	/**
+	 * Searches the list of items requested and returns true if all have ben filled
+	*/
+	bool CustomerOrder::isFilled() const {
 		return false;
 	}
 
-	bool CustomerOrder::isItemFilled(const std::string &) const
-	{
+	/**
+	 * Receives an item name
+	 * search the list for that ItemName, return true if all requests for the item has been filled or if the item is not in the lists
+	*/
+	bool CustomerOrder::isItemFilled(const std::string &) const {
 		return false;
 	}
 
-	std::string CustomerOrder::getNameProduct() const
-	{
-		return std::string();
+	/**
+	 * Returns the name of the customer and their product in the following format:
+	 * CUSTOMER [PRODUCT]
+	*/
+	std::string CustomerOrder::getNameProduct() const {
+		return std::string(m_customerName) + "[" + std::string(m_productName) + "]";
 	}
 
+	/**
+	 * Inserts the data for the current object into the ostream
+	 * Has a default showDetail as false
+	 * Displays in the following format:
+	 * Name       [Product]
+	 *            Part 1
+	 *            Part 2
+	 *            Part 3
+	 *            Part 4
+	*/
 	void CustomerOrder::display(std::ostream & os, bool showDetail) const {
 		os << std::left << std::setw(m_fieldWidth) << m_customerName << " [" << m_productName << "]" << std::endl;
+		if (!showDetail) {
 
-		for (size_t i = 0; i < m_numItems; ++i) {
-			os << std::setw(m_fieldWidth) << m_itemInfo[i].s_name << std::endl;
+			for (size_t i = 0; i < m_numItems; ++i) {
+				os << std::setw(m_fieldWidth) << m_itemInfo[i].s_name << std::endl;
+			}
 		}
 	}
-
 }
