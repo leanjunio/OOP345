380f279a8dff17c7fa405bca1ce51514961b8d58
Lean Junio <leanjunio@live.com>
2019-04-03 09:16:59 -0400

    commit

--- OOP345SAA/OOP-Project/MS3/LineManager.cpp
+++ OOP345SAA/OOP-Project/MS3/LineManager.cpp
@@ -2,5 +2,14 @@
 
 namespace sict
 {
-
+	LineManager::LineManager(std::vector<Station*>&, std::vector<size_t>&, std::vector<CustomerOrder>&, size_t, std::ostream &)
+	{
+	}
+	void LineManager::display(std::ostream &) const
+	{
+	}
+	bool LineManager::run(std::ostream &)
+	{
+		return false;
+	}
 }
